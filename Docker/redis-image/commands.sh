63a0888397bcc38eb97924f0427e3db00ddf1fe7
unknown <rmtestard@gmail.com>
2019-04-13 10:39:11 +0200

    build context

--- Docker/redis-image/commands.sh
+++ Docker/redis-image/commands.sh
@@ -1,2 +1,7 @@
+# Dockerfile => Docker Client => Docker Server
+# build command to take a dockerfile and generate an image
+# . set of files that we specify the build context (files that we want to wrap in our container)
 docker build .
+
+
 docker run 650a78bdd97e
