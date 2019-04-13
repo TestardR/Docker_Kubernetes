b4341d3ccd1328c19ab1abf4734e4810525ad2dc
unknown <rmtestard@gmail.com>
2019-04-13 11:01:21 +0200

    taking images

--- Docker/redis-image/commands.sh
+++ Docker/redis-image/commands.sh
@@ -3,5 +3,9 @@
 # . set of files that we specify the build context (files that we want to wrap in our container)
 docker build .
 
-
 docker run 650a78bdd97e
+
+# tag a docker file
+docker -t <Docker_ID>/<Project_Name>:<version> .
+docker build -t testardr/redis:latest .
+docker run testardr/redis
