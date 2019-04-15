649d774d2d4e3b7a2d709a8db368ed0ad376289b
unknown <rmtestard@gmail.com>
2019-04-15 16:30:58 +0200

    specifying working directory

--- Docker/Simple_App/commands.sh
+++ Docker/Simple_App/commands.sh
@@ -1,3 +1,10 @@
 docker build -t testardr/simple_app .
+
 # 8080 route incoming request to this port on local host to... : 8080 the port inside the container
-docker run -p 8080:8080 testardr/simple_app
+docker run -p 8080:8080 testardr/simple_app 
+# app available on localhost:8080
+docker run -p 5000:8080 testardr/simple_app 
+# app available on localhost:5000
+
+# take a look at the internals
+docker run -it testardr/simple_app sh
