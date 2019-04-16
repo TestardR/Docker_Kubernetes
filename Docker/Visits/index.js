83259e82937655e171f98dd1386bc82c0a42ce8b
unknown <rmtestard@gmail.com>
2019-04-16 12:47:42 +0200

    docker-compose

--- Docker/Visits/index.js
+++ Docker/Visits/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  // if no docker use it should sound like host: 'https://myconnectionaddressforredis.com'
+  host: 'redis-server',
+  post: 6379
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
--- Docker/commands.sh
+++ Docker/commands.sh
@@ -1,1 +1,11 @@
 docker build -t testardr/visits:latest .
+docker run redis
+=> When you have two containers, you have to use compose, look below
+
+
+In the compose world: 
+If you want to rebuild an image : 
+docker-compose up --build
+In our case, we want to run our container : 
+docker-compose up
+
