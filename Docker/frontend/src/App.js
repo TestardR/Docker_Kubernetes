0d253122fd53b5b70afd0e438ee65b02246bdfc4
unknown <rmtestard@gmail.com>
2019-04-18 17:19:28 +0200

    production build with docker

--- Docker/frontend/src/App.js
+++ Docker/frontend/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Banana</p>
+          <p>Bonjour</p>
           <a
             className="App-link"
             href="https://reactjs.org"
