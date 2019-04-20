f0f6d8a2217b357faaa95913b1622efef7856f5f
unknown <rmtestard@gmail.com>
2019-04-20 12:30:51 +0200

    redis Setup

--- Docker/complex/server/index.js
+++ Docker/complex/server/index.js
@@ -23,3 +23,13 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const redisPublisher = redisClient.duplicate();
