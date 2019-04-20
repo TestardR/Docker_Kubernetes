4ec4edad3dd8820b89bbe9749e76363fa322c82f
unknown <rmtestard@gmail.com>
2019-04-20 12:40:05 +0200

    route setup

--- Docker/complex/server/index.js
+++ Docker/complex/server/index.js
@@ -33,3 +33,38 @@ const redisClient = redis.createClient({
 });
 
 const redisPublisher = redisClient.duplicate();
+
+// Express route handlers
+app.get('/', (req, res) => {
+  res.send('Hi');
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, err => {
+  console.log('Listening');
+});
